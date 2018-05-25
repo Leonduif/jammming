ac0abb5067ebd7d773eee7d8ca2fe23af9dbdc21
Leon Duif <leon@travelbird.nl>
2018-05-25 12:59:58 +0200

    until step 35

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,20 +2,27 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-              <h1>Ja<span className="highlight">mmm</span>ing</h1>
-              <div className="App">
-                <SearchBar />
-                <div className="App-playlist">
-                    <SearchResults />
-                    <Playlist />
-                </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: ''
+    };
+  }
+
+  render() {
+      return (
+          <div>
+            <h1>Ja<span className="highlight">mmm</span>ing</h1>
+            <div className="App">
+              <SearchBar />
+              <div className="App-playlist">
+                  <SearchResults searchResults={this.state.searchResults} />
+                  <Playlist />
               </div>
             </div>
-        );
-    }
+          </div>
+      );
+  }
 }
 
 export default App;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Track from './../Track/Track';
 import './TrackList.css';
-
 
 class TrackList extends React.Component {
     render() {
+        const tracks = this.props.searchResults.map(track => {
+            return <Track key={track.id} track={track} />;
+        });
+
         return (
             <div class="TrackList">
-                <!-- You will add a map method that renders a set of Track components  -->
+                {tracks}
             </div>
         );
     }
